f63f31c90e5bb7a866fc546bb6dabbb896d2da0c
Jakub Slawecki <kubaslawecki@gmail.com>
2025-06-30 22:59:25 +0100

    Lesson 2 done

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
