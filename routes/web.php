bd33b626b8387f99279d2312745014ef822566de
Jakub Slawecki <kubaslawecki@gmail.com>
2025-08-08 21:22:55 +0100

    Complete day 18

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->paginate(5);
 
@@ -19,16 +20,19 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function() {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {        
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function() {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -42,6 +46,36 @@ Route::post('/jobs', function() {
     ]);
 
     return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {        
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {        
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect("/jobs/" . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {        
+    Job::findOrFail($id)->delete();
+    return redirect("/jobs");
 });
 
 Route::get('/contact', function () {
