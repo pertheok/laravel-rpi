1ae7629e8365be7759e12a062c09bfe216fc61be
Jakub Slawecki <kubaslawecki@gmail.com>
2025-08-09 21:10:54 +0100

    Complete day 21

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -20,3 +22,10 @@ Route::resource('jobs', JobController::class, [
     // 'except' => ['edit']
     // 'only' => ['index', 'show']
 ]);
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'create']);
