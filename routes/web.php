15922d1fb4ea330488e6d6a8cff36105fac066b3
Jakub Slawecki <kubaslawecki@gmail.com>
2025-08-09 21:44:53 +0100

    Complete day 22

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,5 @@ Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::get('/login', [SessionController::class, 'create']);
-Route::post('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
